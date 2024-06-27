3a477bedbf413ab2eebb641f52ab6b2c7af766d8
Phiraphat <Phiraphat.wo@rmuti.ac.th>
2024-06-27 11:32:54 +0700

    golf

---
+++ golf.py
@@ -0,0 +1,20 @@
+name = "Phiraphat" 
+print (name)  
+# x1 = 89 % 6 # mod การหารเอาเศษเหลือ
+# x2 = 2 ** 4 # การยกกำลัง
+# x3 = 100 // 9 # หารโดยไม่เอาเศษ หรือ ปัดเศษทิ้ง
+# print (x1)
+# print (x2)
+# print (x3)
+x11 = 89 % 6
+x11 += 5
+x33 = x22 = x11
+x22 /= 2
+#print (x11) 
+#print (x22) 
+#print (x33) 
+x1 = x2 = 10
+x1 &= 3
+x2 |= 3
+print(x1)
+print(x2)
